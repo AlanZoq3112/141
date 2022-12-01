2b29e978b0b9750f52d3ec32ddd4036cc7d2eba8
AlanZoq3112 <105748042+AlanZoq3112@users.noreply.github.com>
2022-12-01 09:17:49 -0600

    Add files via upload

---
+++ Archivo/src/app/modules/personal/pages/add-personal/add-personal.component.ts
@@ -0,0 +1,71 @@
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, OnInit } from '@angular/core';
+import { PersonalService } from '../../service/personal.service';
+import { Personal } from '../../types/personla';
+
+
+@Component({
+  selector: 'app-add-personal',
+  templateUrl: './add-personal.component.html'
+})
+export class AddPersonalComponent implements OnInit {
+  personal: Personal = {
+    id: 0,
+    name: "",
+    surname: "",
+    lastname: "",
+    birthday: "",
+    salary: 0.0,
+    position: {
+      id: 0,
+      position: "",
+      description: ""
+    },
+    user: undefined
+  };
+
+  positions: any[] = [];
+
+  constructor(private perosnalService: PersonalService,
+    public modalRef: DialogRef<AddPersonalComponent>) { }
+
+  ngOnInit(): void {
+    //BACK-END -> servicio que obtenga las posiciones
+    this.perosnalService.findAllPositions()
+    .subscribe((Response)=>{
+      this.positions = Response;
+    })
+    //Realizar la peticion hacia su servicio de postions
+    //Asignar las positions a nuestra valriable this.positions
+  }
+
+  savePersonal() {
+    this.perosnalService.save(this.personal)
+      .subscribe((response) => {
+        this.personal = {
+          id: 0,
+          name: "",
+          surname: "",
+          lastname: "",
+          birthday: "",
+          salary: 0.0,
+          position: {
+            id: 0,
+            position: "",
+            description: ""
+          },
+          user: undefined
+        };
+
+        this.modalRef.close();
+
+        
+        //Validar si la persona fue registrada correctamente
+        //Si -> Cerrar modal, limpar el formulario y actualizar la consulta general del personal
+
+      })
+  }
+
+}
+// Subscribe espera la peticion
+
